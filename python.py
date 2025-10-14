9290a40773a5021e970cc53f997a322db7c2832f
KHLVN <qkpnicolas@tip.edu.ph>
2025-10-14 20:48:23 +0800

    Reorganized structure of files

--- python.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import tensorflowjs as tfjs
-
-# Load your Keras model
-model = tf.keras.models.load_model('your_model.h5')
-
-# Convert and save the model in TensorFlow.js format
-tfjs.converters.save_keras_model(model, 'backend\src\dl\weather_model.h5')
